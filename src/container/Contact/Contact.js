84a4c522ef2a4917f35224719a7f73c4a006623e
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-02 17:27:24 -0400

    added click event handler

--- src/container/Contact/Contact.js
+++ src/container/Contact/Contact.js
@@ -2,12 +2,19 @@ import React, {Component} from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component{
+	state = {}
+
+	onShowClick = (id, e) =>{
+		console.log(id, e.target);
+	}
+
 	render(){
 		const {name, email, phone} = this.props.contact;
 
 		return(
 			<div className="card card-body mb-3">
-				<h4>{name} <i className="fas fa-sort-down"></i></h4>
+				<h4>{name} <i onClick={(e) => this.onShowClick(1, e)} className="fas fa-sort-down"></i></h4>
+
 				<ul className="list-group">
 					<li className="list-group-item">Email: {email}</li>
 					<li className="list-group-item">Phone: {phone}</li>
