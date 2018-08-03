cfcb6eee524b14586009e3b8016d34f1d36db619
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-03 11:59:58 -0400

    toggle feature added for contact info

--- src/container/Contact/Contact.js
+++ src/container/Contact/Contact.js
@@ -2,23 +2,29 @@ import React, {Component} from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component{
-	state = {}
+	state = {
+		showContactInfo: false
+	}
 
 	onShowClick = (id, e) =>{
-		console.log(id, e.target);
+		this.setState({showContactInfo: !this.state.showContactInfo})
 	}
 
 	render(){
 		const {name, email, phone} = this.props.contact;
+		const {showContactInfo} = this.state;
 
 		return(
 			<div className="card card-body mb-3">
 				<h4>{name} <i onClick={(e) => this.onShowClick(1, e)} className="fas fa-sort-down"></i></h4>
-
-				<ul className="list-group">
-					<li className="list-group-item">Email: {email}</li>
-					<li className="list-group-item">Phone: {phone}</li>
-				</ul>
+				{
+					showContactInfo ? 
+					<ul className="list-group">
+						<li className="list-group-item">Email: {email}</li>
+						<li className="list-group-item">Phone: {phone}</li>
+					</ul>
+					: null
+				}
 			</div>
 		);
 	};
